c4b7fd294dda6b8a87ca26a1a2f00efa9fdf4b34
Gideon Sanni <gideonyte@hotmail.com>
2021-03-09 12:42:36 -0700

    new test server  IP

--- src/Application/Services/BaseUrlService.ts
+++ src/Application/Services/BaseUrlService.ts
@@ -1,11 +1,11 @@
 const getBaseUrl = () => {
   // return "http://54.174.78.245/forecast";
-  return "http://34.205.77.46/forecast";
+  return "https://34.205.77.46/forecast";
 };
 
 export const getBaseAuthUrl = () => {
   // return "http://54.174.78.245/auth";
-  return "http://34.205.77.46/auth";
+  return "https://34.205.77.46/auth";
 };
 
 export default getBaseUrl;
