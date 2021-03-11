27beccaaba871d61e8a5de31638d311b9476b476
Gideon Sanni <gideonyte@hotmail.com>
2021-03-11 10:03:26 -0700

    new test server  IP

--- src/Application/Services/BaseUrlService.ts
+++ src/Application/Services/BaseUrlService.ts
@@ -1,10 +1,12 @@
 const getBaseUrl = () => {
-  return "http://54.174.78.245/forecast";
+  return "https://apex.syncware.io/forecast";
+  // return "https://54.174.78.245/forecast";
   // return "http://34.205.77.46/forecast";
 };
 
 export const getBaseAuthUrl = () => {
-  return "http://54.174.78.245/auth";
+  return "https://apex.syncware.io/auth";
+  // return "https://54.174.78.245/auth";
   // return "http://34.205.77.46/auth";
 };
 
