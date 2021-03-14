3acdd8d897953db81f2a8935c06026378315b4bc
Gideon Sanni <gideonyte@hotmail.com>
2021-03-14 11:23:01 -0600

    changed IP for dev branch

--- src/Application/Services/BaseUrlService.ts
+++ src/Application/Services/BaseUrlService.ts
@@ -3,7 +3,8 @@ const getBaseUrl = () => {
   // return "https://54.174.78.245/forecast";
   // return "http://34.205.77.46/forecast";
 
-  return "http://88382c99b104.ngrok.io/api/forecast";
+  // return "http://88382c99b104.ngrok.io/api/forecast";
+  return "https://107.23.121.64/forecast";
 };
 
 export const getBaseAuthUrl = () => {
@@ -11,7 +12,8 @@ export const getBaseAuthUrl = () => {
   // return "https://54.174.78.245/auth";
   // return "http://34.205.77.46/auth";
 
-  return "http://88382c99b104.ngrok.io/api/forecast";
+  // return "http://88382c99b104.ngrok.io/api/forecast";
+  return "https://107.23.121.64/auth";
 };
 
 export default getBaseUrl;
