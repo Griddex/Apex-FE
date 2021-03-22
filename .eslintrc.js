412a0a2b4ee6f4d03c98451fe2cd952648525993
Gideon Sanni <gideonyte@hotmail.com>
2021-03-21 21:42:14 -0600

    updated react-data-griddex

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,8 @@ module.exports = {
       rules: {
         "react/prop-types": "off",
         "react/display-name": "off",
-        "@typescript-eslint/no-empty-function": "off",
         "react/display-name": "off",
+        "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
