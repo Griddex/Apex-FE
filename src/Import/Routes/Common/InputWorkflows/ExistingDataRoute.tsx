b1d095c403f76a566e47111aec80cebe9853f735
Gideon Sanni <gideonyte@hotmail.com>
2021-03-22 22:19:06 -0600

    updated  existingdataroute

--- src/Import/Routes/Common/InputWorkflows/ExistingDataRoute.tsx
+++ src/Import/Routes/Common/InputWorkflows/ExistingDataRoute.tsx
@@ -107,6 +107,10 @@ export default function ExistingDataRoute<
     shouldDispatch: false,
     apexCheckboxAction: handleCheckboxChange,
   });
+  console.log(
+    "Logged output --> ~ file: ExistingDataRoute.tsx ~ line 110 ~ ApexCheckboxColumn",
+    ApexCheckboxColumn
+  );
 
   const generateColumns = () => {
     const columns: Column<TRow>[] = [
