cae4b873bcab36a37e33fe2ec9b686c43297bc3e
Gideon Sanni <gideonyte@hotmail.com>
2021-03-27 15:26:45 -0600

    updated getDuplicates

--- src/Application/Utils/GetDuplicates.ts
+++ src/Application/Utils/GetDuplicates.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 const getDuplicates = (duplicatesArray: React.Key[]) => {
-  let duplicates = duplicatesArray.reduce(
+  const duplicates = duplicatesArray.reduce(
     (acc: React.Key[], currentValue, index, array) => {
       if (array.indexOf(currentValue) != index && !acc.includes(currentValue))
         acc.push(currentValue);
