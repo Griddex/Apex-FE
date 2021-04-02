0fca8a2fc976ea2b9341e11b68c81a8bc0ab7f8a
Gideon Sanni <gideonyte@hotmail.com>
2021-04-02 11:08:23 -0600

    updated urlservice

--- src/Application/Services/BaseUrlService.ts
+++ src/Application/Services/BaseUrlService.ts
@@ -1,10 +1,12 @@
 const getBaseUrl = () => {
   return "https://apex.syncware.io/forecast";
+  // return "https://dev.syncware.io/forecast";
   // return "http://2010d45247c1.ngrok.io/api/forecast";
 };
 
 export const getBaseAuthUrl = () => {
   return "https://apex.syncware.io/auth";
+  // return "https://dev.syncware.io/auth";
   // return "http://2010d45247c1.ngrok.io/api/forecast";
 };
 
