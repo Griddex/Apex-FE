38a66ab387275da30825d29800ac85a2c1d8cf5c
Gideon Sanni <gideonyte@hotmail.com>
2021-07-20 19:37:04 -0600

    added axios interceptors

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -1,7 +1,39 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(
+  (config) => {
+    console.log(
+      "Logged output --> ~ file: AuthService.ts ~ line 5 ~ config",
+      config
+    );
+    return config;
+  },
+  (error) => {
+    console.log(
+      "Logged output --> ~ file: AuthService.ts ~ line 8 ~ error",
+      error
+    );
+    return Promise.reject(error);
+  }
+);
 
+axios.interceptors.response.use(
+  (reponse) => {
+    console.log(
+      "Logged output --> ~ file: AuthService.ts ~ line 5 ~ reponse",
+      reponse
+    );
+    return reponse;
+  },
+  (error) => {
+    console.log(
+      "Logged output --> ~ file: AuthService.ts ~ line 8 ~ error",
+      error
+    );
+    return Promise.reject(error);
+  }
+);
 export const post = (url: string, data: any, config: AxiosRequestConfig) => {
   return axios.post(url, data, config);
 };
