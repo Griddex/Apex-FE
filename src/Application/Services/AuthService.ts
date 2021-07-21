70937be1f7bf0420d44064250a2e0f8618e881ad
Gideon Sanni <gideonyte@hotmail.com>
2021-07-20 19:24:32 -0600

    Configured Axios with credentials=true at global instance

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosRequestConfig } from "axios";
 
+const apexAxios = axios.create({ withCredentials: true });
+
 export const post = (url: string, data: any, config: AxiosRequestConfig) => {
-  return axios.post(url, data, config);
+  return apexAxios.post(url, data, config);
 };
 
 export const get = (url: string, config: AxiosRequestConfig) => {
-  return axios.get(url, config);
+  return apexAxios.get(url, config);
 };
 
 export const deleteData = (url: string, config: AxiosRequestConfig) => {
-  return axios.delete(url, config);
+  return apexAxios.delete(url, config);
 };
