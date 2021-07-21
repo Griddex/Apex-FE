bd2f9956f5af055a2336586250fdbf663d5ae24a
Gideon Sanni <gideonyte@hotmail.com>
2021-07-21 00:16:32 -0600

    fixed saga

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -1,13 +1,21 @@
 import axios from "axios";
 
+const apexAxios = axios.create({
+  withCredentials: true,
+});
+console.log(
+  "Logged output --> ~ file: AuthService.ts ~ line 6 ~ apexAxios",
+  apexAxios
+);
+
 export const post = (url: string, data: any, config: any) => {
-  return axios.post(url, data, config);
+  return apexAxios.post(url, data, config);
 };
 
 export const get = (url: string, config: any) => {
-  return axios.get(url, config);
+  return apexAxios.get(url, config);
 };
 
 export const deleteData = (url: string, config: any) => {
-  return axios.delete(url, config);
+  return apexAxios.delete(url, config);
 };
