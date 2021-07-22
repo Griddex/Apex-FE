8302cddd06897d73b7a1ec0d2c6cb6f01cfac6ce
Gideon Sanni <gideonyte@hotmail.com>
2021-07-21 23:48:05 -0600

    Using global Axios instance to set headers

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -9,17 +9,21 @@ const headers = {
   "X-Access-Token": token,
 };
 
+const apexAxios = axios.create({
+  headers,
+});
+
 export const post = (url: string, data: any, config: AxiosRequestConfig) => {
   // if (url.endsWith("/signin"))
   //   return axios.post(url, data, { ...config, ...headers });
   //   else return axios.post(url, data, config);
-  return axios.post(url, data, { ...config, ...headers });
+  return apexAxios.post(url, data, { ...config, ...headers });
 };
 
 export const get = (url: string, config: AxiosRequestConfig) => {
-  return axios.get(url, { ...config, ...headers });
+  return apexAxios.get(url, { ...config, ...headers });
 };
 
 export const deleteData = (url: string, config: AxiosRequestConfig) => {
-  return axios.delete(url, { ...config, ...headers });
+  return apexAxios.delete(url, { ...config, ...headers });
 };
