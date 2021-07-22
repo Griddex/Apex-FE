54f109eef6185348e9aeed2080faa4f65965c28e
Gideon Sanni <gideonyte@hotmail.com>
2021-07-22 17:10:28 -0600

    Removed axios instance config

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -1,39 +1,27 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const apexAxios = axios.create({
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    "Accept-Language": "en-US, en;q=0.8",
-  },
-});
+// const apexAxios = axios.create({
+//   headers: {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+//     "Accept-Language": "en-US, en;q=0.8",
+//   },
+// });
 
-apexAxios.interceptors.request.use(
+axios.interceptors.request.use(
   (config) => {
     const newConfig = {
       ...config,
       headers: {
         ...config.headers,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Accept-Language": "en-US, en;q=0.8",
         "X-Access-Token": sessionStorage.getItem("token"),
       },
     };
-    console.log(
-      "Logged output --> ~ file: AuthService.ts ~ line 21 ~ newConfig",
-      newConfig
-    );
+
     return newConfig;
-  },
-  (err) => {
-    return Promise.reject();
-  }
-);
-apexAxios.interceptors.response.use(
-  (config) => {
-    console.log(
-      "Logged output --> ~ file: AuthService.ts ~ line 28 ~ config",
-      config
-    );
-    return config;
   },
   (err) => {
     return Promise.reject();
@@ -41,13 +29,13 @@ apexAxios.interceptors.response.use(
 );
 
 export const post = (url: string, data: any, config: AxiosRequestConfig) => {
-  return apexAxios.post(url, data, config);
+  return axios.post(url, data, config);
 };
 
 export const get = (url: string, config: AxiosRequestConfig) => {
-  return apexAxios.get(url, config);
+  return axios.get(url, config);
 };
 
 export const deleteData = (url: string, config: AxiosRequestConfig) => {
-  return apexAxios.delete(url, config);
+  return axios.delete(url, config);
 };
