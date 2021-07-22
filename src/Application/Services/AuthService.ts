3401914cbaa41267d90c68cf85a230593cc62902
Gideon Sanni <gideonyte@hotmail.com>
2021-07-22 16:53:31 -0600

    using axios interceptors for token in authservice

--- src/Application/Services/AuthService.ts
+++ src/Application/Services/AuthService.ts
@@ -1,47 +1,53 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const token = sessionStorage.getItem("token");
-const headers = {
-  Accept: "application/json",
-  "Content-Type": "application/json",
-  "Accept-Language": "en-US, en;q=0.8",
-  // Authorization: `Bearer ${token}`,
-  "X-Access-Token": token,
-};
-
 const apexAxios = axios.create({
-  headers,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Accept-Language": "en-US, en;q=0.8",
+  },
 });
 
 apexAxios.interceptors.request.use(
   (config) => {
+    const newConfig = {
+      ...config,
+      headers: {
+        ...config.headers,
+        "X-Access-Token": localStorage.getItem("token"),
+      },
+    };
     console.log(
-      "Logged output --> ~ file: AuthService.ts ~ line 17 ~ config",
+      "Logged output --> ~ file: AuthService.ts ~ line 21 ~ newConfig",
+      newConfig
+    );
+    return newConfig;
+  },
+  (err) => {
+    return Promise.reject();
+  }
+);
+apexAxios.interceptors.response.use(
+  (config) => {
+    console.log(
+      "Logged output --> ~ file: AuthService.ts ~ line 28 ~ config",
       config
     );
-
     return config;
   },
   (err) => {
-    console.log(
-      "Logged output --> ~ file: AuthService.ts ~ line 21 ~ err",
-      err
-    );
     return Promise.reject();
   }
 );
 
 export const post = (url: string, data: any, config: AxiosRequestConfig) => {
-  // if (url.endsWith("/signin"))
-  //   return axios.post(url, data, { ...config, ...headers });
-  //   else return axios.post(url, data, config);
-  return apexAxios.post(url, data, { ...config, ...headers });
+  return apexAxios.post(url, data, config);
 };
 
 export const get = (url: string, config: AxiosRequestConfig) => {
-  return apexAxios.get(url, { ...config, ...headers });
+  return apexAxios.get(url, config);
 };
 
 export const deleteData = (url: string, config: AxiosRequestConfig) => {
-  return apexAxios.delete(url, { ...config, ...headers });
+  return apexAxios.delete(url, config);
 };
